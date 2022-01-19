73aa86cf7365c942c5b473aa42a50b3e4bd720e2
hmshuvo314 <hmshuvo314@gmail.com>
2022-01-19 20:13:16 +0600

    added to the app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from chat import get_response
 
 
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
